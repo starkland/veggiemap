d288ab12907130d7ae03ca3a0d507a87d6ba9e14
thulioph <thulioph@gmail.com>
2017-02-10 01:17:04 -0300

    Ajusta as cores do mapa e adiciona método para exibir um raio ao redor do marcador.

--- js/services/maps.js
+++ js/services/maps.js
@@ -26,15 +26,24 @@
       var lat = position.coords.latitude;
       var lng = position.coords.longitude;
 
+      var veggieStyle = obj.StyleObj();
+
       var latlng = new google.maps.LatLng(lat, lng);
       var mapElement = document.getElementById('map-canvas');
       var mapOptions = {
-        zoom: 10,
+        zoom: 14,
         center: latlng,
-        mapTypeId: 'roadmap'
+        mapTypeId: 'roadmap',
+        zoomControlOptions: {
+          style: google.maps.ZoomControlStyle.SMALL,
+          position: google.maps.ControlPosition.LEFT_BOTTOM
+        }
       };
 
       map = new google.maps.Map(mapElement, mapOptions);
+
+      map.mapTypes.set('veggie_map', veggieStyle);
+      map.setMapTypeId('veggie_map');
 
       infowindow = new google.maps.InfoWindow;
       geocoder = new google.maps.Geocoder();
@@ -86,6 +95,26 @@
       });
     }
 
+    function StyleObj() {
+      var styles;
+
+      styles = [{"featureType":"administrative","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"administrative","elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative.country","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"administrative.land_parcel","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#fafafa"}]},{"featureType":"landscape","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"landscape.man_made","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"landscape.man_made","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"poi.park","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#79e888"},{"saturation":"-21"},{"lightness":"-31"},{"gamma":"3.95"}]},{"featureType":"poi.park","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"poi.park","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"visibility":"on"},{"color":"#fbbc45"},{"weight":"1.00"}]},{"featureType":"road","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"labels.text","stylers":[{"visibility":"on"},{"color":"#c5c5c5"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"},{"weight":"2.85"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"road.highway","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"road.arterial","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"road.arterial","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#59bddc"},{"visibility":"on"}]},{"featureType":"water","elementType":"labels.text","stylers":[{"visibility":"off"}]}];
+
+      return new google.maps.StyledMapType(styles, { name: 'Veggie Map' });
+    }
+
+    function DisplayRadius() {
+      return new google.maps.Circle({
+        map: map,
+        radius: 200,
+        fillColor: '##00D1B2',
+        fillOpacity: 0.15,
+        strokeOpacity: 0.51,
+        strokeColor: '##00D1B2',
+        strokeWeight: 1
+      });
+    }
+
     // ====
 
     obj = {
@@ -94,7 +123,8 @@
       SuccessLocation,
       ErrorLocation,
       AddMarker,
-      Geocoder
+      Geocoder,
+      StyleObj
     };
 
     // ====
