756f1929d1b40ad0985e7f86e5088bbde5f63a92
thulioph <thulioph@gmail.com>
2017-03-03 04:01:29 -0300

    Continua implementação do mapa.

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -23,16 +23,16 @@
 
     methods: {
       updateVeggie(data) {
-        let { veggie } = Object.values(data.veggies)[0];
+        this.veggies_array = data.veggies_array;
 
-        L
-          .marker([veggie.location[0], veggie.location[1]])
-          .addTo(this.map)
-          .bindPopup(`<h4>${veggie.name}</h4>
-              <small>${veggie.address}</small>`)
-          .openPopup();
-
-        // this.veggies_array.push(data.veggies);
+        for(let item of data.veggies_array) {
+          L
+            .marker([item.veggie.location[0], item.veggie.location[1]])
+            .addTo(this.map)
+            .bindPopup(`<h4>${item.veggie.name}</h4>
+                <small>${item.veggie.address}</small>`)
+            .openPopup();
+        }
       },
 
       updateVeggies(data) {
