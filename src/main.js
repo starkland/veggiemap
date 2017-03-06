98d15a3c152d33121d223a0a284d377a38de9ad2
Thulio Philipe <thulioph@gmail.com>
2017-03-06 18:28:25 -0300

    Utiliza um arquivo externo para melhor gerenciar as rotas da aplicação.

--- src/main.js
+++ src/main.js
@@ -1,35 +1,17 @@
 import Vue from 'vue';
-import VueRouter from 'vue-router';
 import VueResource from 'vue-resource';
 import VueProgressBar from 'vue-progressbar';
 
 import vgApp from './views/App.vue';
-import vgContact from './views/Contact.vue';
 
+import router from './router/index';
 
-Vue.use(VueRouter);
 Vue.use(VueResource);
 
 Vue.use(VueProgressBar, {
   color: 'rgb(143, 255, 199)',
   failedColor: 'red',
   height: '2px'
-});
-
-const routes = [
-  {
-    path: '/',
-    component: vgApp
-  },
-  {
-    path: '/contato',
-    component: vgContact
-  }
-];
-
-const router = new VueRouter({
-  routes,
-  mode: 'history' // removes the # from URL
 });
 
 new Vue({
