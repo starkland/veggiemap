fd454f8049da00bc8e9fda2b3f9a6606846aa8da
thulioph <thulioph@gmail.com>
2017-04-03 16:05:37 -0300

    Realiza o refactor do location service.

--- src/assets/js/Location.js
+++ src/assets/js/Location.js
@@ -1,31 +1,25 @@
-import Events from '../../events/all';
-
 class Location {
   constructor() {
     this.latLng = [];
   }
 
-  currentPosition() {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      this.latLng[0] = position.coords.latitude;
-      this.latLng[1] = position.coords.longitude;
-
-      this.dispatch();
-    }.bind(this), function(err) {
-      console.error(err);
-
-      // Recife - Marco Zero
-      this.latLng[0] = '-8.0631534';
-      this.latLng[1] = '-34.8733055';
-
-      this.dispatch();
-    }.bind(this));
+  get() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      this._success(position);
+    }, (error) => {
+      this._error(error);
+    });
   }
 
-  dispatch() {
-    Events.$emit('location_ok', {
-      position: this.position()
-    });
+  _success(position) {
+    this.latLng[0] = position.coords.latitude;
+    this.latLng[1] = position.coords.longitude;
+  }
+
+  _error() {
+    // Recife - Marco Zero
+    this.latLng[0] = '-8.0631534';
+    this.latLng[1] = '-34.8733055';
   }
 
   position() {
