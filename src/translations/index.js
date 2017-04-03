bb7d6fc563c527e14c8934588596f21ee110f684
thulioph <thulioph@gmail.com>
2017-04-03 01:51:14 -0300

    Adiciona arquivo de translations.

---
+++ src/translations/index.js
@@ -0,0 +1,19 @@
+const entry = {
+  en: {
+    card: {
+      header: {
+        title: 'Add Veggie'
+      }
+    }
+  },
+
+  pt: {
+    card: {
+      header: {
+        title: 'Adicionar Veggie'
+      }
+    }
+  }
+}
+
+export default entry;
